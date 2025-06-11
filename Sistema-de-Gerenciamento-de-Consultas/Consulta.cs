1473d2077fdf53f7ee582ab5c383f2243db94b35
GraziellaPereira <graziellapereira52@gmail.com>
2025-06-11 12:13:19 -0300

    Enviado codigo inicial

---
+++ Sistema-de-Gerenciamento-de-Consultas/Consulta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Gerenciamento_de_Consultas
+{
+    public class Consulta
+    {
+        public string Nome {  get; set; }
+
+    }
+}
---
+++ Sistema-de-Gerenciamento-de-Consultas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sistema_de_Gerenciamento_de_Consultas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t --- SISTEMA DE GERENCIAMENTO DE CONSULTAS MÉDICAS SGCM ---");
+            Console.WriteLine("\n Iniciando o sistema...");
+            bool executando = true;
+            while (executando) {
+                Console.WriteLine("Digite o número da operação que deseja realizar: ");
+                Console.WriteLine("1- Agendar consulta");
+                Console.WriteLine();
+            }
+        }
+    }
+}
